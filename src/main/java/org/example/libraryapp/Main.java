95e27f2ee8e11e9ca92e946aa7e464064a63e0cf
Sieg <csow20738@iesjoanramis.org>
2024-06-04 15:56:41 +0200

    Proyecto modificado 2

--- src/main/java/org/example/libraryapp/Main.java
+++ src/main/java/org/example/libraryapp/Main.java
@@ -7,8 +7,8 @@ import javafx.stage.Stage;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main{
-    public static void main(String[] args) {
+public class Main extends Application{
+    public static void main(String[] args){
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
@@ -20,4 +20,8 @@ public class Main{
         }
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
